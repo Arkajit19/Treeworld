4400092ef8b67bffceee17794eb55295c10ff66f
Arkajit <59366904+Arkajit19@users.noreply.github.com>
2022-09-24 09:17:06 +0530

    first change

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Treeworld
+{
+    class Goal
+    {
+
+        static void Main(String[] args)
+        {
+            /*Console.WriteLine("Enter first number : ");
+            int b1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number : ");
+            int c1 = Convert.ToInt32(Console.ReadLine());
+            int d1 = b1 + c1;
+            Console.WriteLine("The sum is : " + d1);
+            Console.ReadKey();*/
+            //Console.WriteLine("Hello\bWorld");
+            //Console.ReadKey();
+
+            //int a1 = 67;
+            //while (a1 < 80)
+            //{
+            //    Console.WriteLine("The value is : {0}", a1);
+            //    a1++;
+            //}
+
+            //for(int b1=2; b1<20; b1++)
+            //{
+            //    Console.WriteLine("The value is : {0}", b1);
+
+            //}
+            int a1 = 10;
+            do
+            {
+                Console.WriteLine("the value is {0}" , a1);
+                a1++;
+            } while (a1 < 20);
+            Console.ReadKey();
+
+        }
+    }
+}
+
